b4922628c48525e0369867a2670f0410fc927134
omnitechintegrators <omnitech.integrators1@gmail.com>
2026-03-19 10:51:38 +0530

    updated server port

--- server.js
+++ server.js
@@ -58,7 +58,6 @@ app.use(
 "/uploads",
 
 express.static("uploads")
-
 );
 
 app.use("/api/blog", blogRoutes);
@@ -78,10 +77,8 @@ app.use("/api/comments", commentRoutes);
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
-
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("Server running");
-});
+  console.log("Server is running");
+});sh
