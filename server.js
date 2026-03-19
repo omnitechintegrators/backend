6e41dc60d324bcc8772b95b875c906840d371bae
omnitechintegrators <omnitech.integrators1@gmail.com>
2026-03-19 12:47:47 +0530

    fix render timeout issue

--- server.js
+++ server.js
@@ -97,6 +97,6 @@ mongoose.connect(process.env.MONGO_URI)
 // ================= SERVER =================
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
