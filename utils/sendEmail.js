7b9f7511bf03a4e198931e9051c942722c6ff79a
omnitechintegrators <omnitech.integrators1@gmail.com>
2026-03-19 17:25:28 +0530

    fix email config

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,23 +3,12 @@ import nodemailer from "nodemailer";
 const sendEmail = async (to, subject, html, attachmentPath = null) => {
 
 try {
-
 const transporter = nodemailer.createTransport({
-
-host: process.env.EMAIL_HOST,
-
-port: process.env.EMAIL_PORT,
-
-secure: true,
-
-auth: {
-
-user: process.env.EMAIL_USER,
-
-pass: process.env.EMAIL_PASS,
-
-},
-
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 
