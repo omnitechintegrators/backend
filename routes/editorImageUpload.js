69deced65ee2dbe37801668f99324eed7c1222c9
omnitechintegrators <omnitech.integrators1@gmail.com>
2026-03-24 13:59:11 +0530

    backend ready

--- routes/editorImageUpload.js
+++ routes/editorImageUpload.js
@@ -1,57 +1,33 @@
 import express from "express";
-
 import multer from "multer";
-
 import fs from "fs";
-
 
 const router = express.Router();
 
-
 const storage = multer.diskStorage({
-
-destination: (req, file, cb) => {
-
-const dir = "uploads/blog";
-
-fs.mkdirSync(dir, { recursive: true });
-
-cb(null, dir);
-
-},
-
-filename: (req, file, cb) => {
-
-cb(null, Date.now() + "-" + file.originalname);
-
-}
-
+  destination: (req, file, cb) => {
+    const dir = "uploads/blog";
+    fs.mkdirSync(dir, { recursive: true });
+    cb(null, dir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  }
 });
-
 
 const upload = multer({ storage });
 
+router.post("/", upload.single("image"), (req, res) => {
 
-router.post(
-"/",
-upload.single("image"),
-(req, res) => {
+  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/blog/${req.file.filename}`;
 
-res.json({
-
-success: 1,
-
-file: {
-
-url:
-`http://localhost:5000/uploads/blog/${req.file.filename}`
-
-}
+  res.json({
+    success: 1,
+    file: {
+      url: imageUrl
+    }
+  });
 
 });
-
-}
-);
-
 
 export default router;
